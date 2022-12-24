3b0bd75ea07d4d42704ae8c3452a5e155c4cc73c
ignace <ignace@otainsight.com>
2022-12-24 16:31:59 +0100

    Add ts signature

--- components-legacy/ember-tsparticles/ember-tsparticles/addon/modifiers/particles.ts
+++ components-legacy/ember-tsparticles/ember-tsparticles/addon/modifiers/particles.ts
@@ -1,122 +1,26 @@
-import Modifier from 'ember-modifier';
-import { tsParticles } from 'tsparticles-engine';
+import Modifier, { NamedArgs, PositionalArgs } from 'ember-modifier';
+import { Options, tsParticles } from 'tsparticles-engine';
 
 import { registerDestructor } from '@ember/destroyable';
 
-export default class ParticlesModifier extends Modifier {
-  async modify(element: Element) {
+interface ParticlesModifierSignature {
+  Args: {
+    Positional: [];
+    Named: {
+      options: Options;
+    };
+  };
+}
+
+export default class ParticlesModifier extends Modifier<ParticlesModifierSignature> {
+  async modify(
+    element: Element,
+    _: PositionalArgs<ParticlesModifierSignature>,
+    { options }: NamedArgs<ParticlesModifierSignature>
+  ) {
     // const { tsParticles } = await import('tsparticles-engine');
     console.log(element.id);
-    const particlesCanvas = await tsParticles.load(element.id, {
-      particles: {
-        number: {
-          value: 52,
-          density: {
-            enable: true,
-            value_area: 631.3280775270874,
-          },
-        },
-        color: {
-          value: '#fff',
-        },
-        shape: {
-          type: 'circle',
-          stroke: {
-            width: 0,
-            color: '#000000',
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: 'img/github.svg',
-            width: 100,
-            height: 100,
-          },
-        },
-        opacity: {
-          value: 0.5,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false,
-          },
-        },
-        size: {
-          value: 5,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false,
-          },
-        },
-        line_linked: {
-          enable: false,
-          distance: 500,
-          color: '#ffffff',
-          opacity: 0.4,
-          width: 2,
-        },
-        move: {
-          enable: true,
-          speed: 1.5,
-          direction: 'bottom',
-          random: false,
-          straight: false,
-          out_mode: 'out',
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200,
-          },
-        },
-      },
-      interactivity: {
-        detect_on: 'canvas',
-        events: {
-          onhover: {
-            enable: false,
-            mode: 'bubble',
-          },
-          onclick: {
-            enable: true,
-            mode: 'repulse',
-          },
-          resize: true,
-        },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 0.5,
-            },
-          },
-          bubble: {
-            distance: 400,
-            size: 4,
-            duration: 0.3,
-            opacity: 1,
-            speed: 3,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-      },
-      retina_detect: true,
-    });
+    const particlesCanvas = await tsParticles.load(element.id, options);
 
     registerDestructor(this, () => {
       particlesCanvas?.destroy();
