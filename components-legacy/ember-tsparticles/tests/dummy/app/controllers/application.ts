f324775139f1c9d8c39a916d4f55acc2a8feeea6
ignace <ignace@otainsight.com>
2022-12-26 16:50:54 +0100

    Use preset snow

--- components-legacy/ember-tsparticles/tests/dummy/app/controllers/application.ts
+++ components-legacy/ember-tsparticles/tests/dummy/app/controllers/application.ts
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { Engine } from 'tsparticles-engine';
-import { loadConfettiPreset } from 'tsparticles-preset-confetti';
+import { loadSnowPreset } from 'tsparticles-preset-snow';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
+  @tracked isConfettiVisible = false;
+
   options = {
     fullScreen: {
       enable: false,
@@ -48,134 +51,130 @@ export default class ApplicationController extends Controller {
   };
 
   confetti = {
-    preset: 'confetti',
+    // duration: 3,
+    fullScreen: {
+      zIndex: 1,
+    },
+    emitters: {
+      life: {
+        count: 1,
+        duration: 3,
+      },
+      position: {
+        x: 50,
+        y: 100,
+      },
+      rate: {
+        quantity: 5,
+        delay: 0.15,
+      },
+    },
+    particles: {
+      color: {
+        value: ['#1E00FF', '#FF0061', '#E1FF00', '#00FF9E'],
+      },
+      move: {
+        decay: 0.05,
+        direction: 'top',
+        enable: true,
+        gravity: {
+          enable: true,
+        },
+        outModes: {
+          top: 'none',
+          default: 'destroy',
+        },
+        speed: {
+          min: 50,
+          max: 100,
+        },
+      },
+      number: {
+        value: 0,
+      },
+      opacity: {
+        value: 1,
+      },
+      rotate: {
+        value: {
+          min: 0,
+          max: 360,
+        },
+        direction: 'random',
+        animation: {
+          enable: true,
+          speed: 30,
+        },
+      },
+      tilt: {
+        direction: 'random',
+        enable: true,
+        value: {
+          min: 0,
+          max: 360,
+        },
+        animation: {
+          enable: true,
+          speed: 30,
+        },
+      },
+      size: {
+        value: 3,
+        animation: {
+          enable: true,
+          startValue: 'min',
+          count: 1,
+          speed: 16,
+          sync: true,
+        },
+      },
+      roll: {
+        darken: {
+          enable: true,
+          value: 25,
+        },
+        enlighten: {
+          enable: true,
+          value: 25,
+        },
+        enable: true,
+        speed: {
+          min: 5,
+          max: 15,
+        },
+      },
+      wobble: {
+        distance: 30,
+        enable: true,
+        speed: {
+          min: -7,
+          max: 7,
+        },
+      },
+      shape: {
+        type: ['circle', 'square'],
+        options: {},
+      },
+    },
+    responsive: [
+      {
+        maxWidth: 1024,
+        options: {
+          particles: {
+            move: {
+              speed: {
+                min: 33,
+                max: 66,
+              },
+            },
+          },
+        },
+      },
+    ],
   };
 
-  // confetti = {
-  //   // duration: 3,
-  //   fullScreen: {
-  //     zIndex: 1,
-  //   },
-  //   emitters: {
-  //     life: {
-  //       count: 1,
-  //       duration: 3,
-  //     },
-  //     position: {
-  //       x: 50,
-  //       y: 100,
-  //     },
-  //     rate: {
-  //       quantity: 5,
-  //       delay: 0.15,
-  //     },
-  //   },
-  //   particles: {
-  //     color: {
-  //       value: ['#1E00FF', '#FF0061', '#E1FF00', '#00FF9E'],
-  //     },
-  //     move: {
-  //       decay: 0.05,
-  //       direction: 'top',
-  //       enable: true,
-  //       gravity: {
-  //         enable: true,
-  //       },
-  //       outModes: {
-  //         top: 'none',
-  //         default: 'destroy',
-  //       },
-  //       speed: {
-  //         min: 50,
-  //         max: 100,
-  //       },
-  //     },
-  //     number: {
-  //       value: 0,
-  //     },
-  //     opacity: {
-  //       value: 1,
-  //     },
-  //     rotate: {
-  //       value: {
-  //         min: 0,
-  //         max: 360,
-  //       },
-  //       direction: 'random',
-  //       animation: {
-  //         enable: true,
-  //         speed: 30,
-  //       },
-  //     },
-  //     tilt: {
-  //       direction: 'random',
-  //       enable: true,
-  //       value: {
-  //         min: 0,
-  //         max: 360,
-  //       },
-  //       animation: {
-  //         enable: true,
-  //         speed: 30,
-  //       },
-  //     },
-  //     size: {
-  //       value: 3,
-  //       animation: {
-  //         enable: true,
-  //         startValue: 'min',
-  //         count: 1,
-  //         speed: 16,
-  //         sync: true,
-  //       },
-  //     },
-  //     roll: {
-  //       darken: {
-  //         enable: true,
-  //         value: 25,
-  //       },
-  //       enlighten: {
-  //         enable: true,
-  //         value: 25,
-  //       },
-  //       enable: true,
-  //       speed: {
-  //         min: 5,
-  //         max: 15,
-  //       },
-  //     },
-  //     wobble: {
-  //       distance: 30,
-  //       enable: true,
-  //       speed: {
-  //         min: -7,
-  //         max: 7,
-  //       },
-  //     },
-  //     shape: {
-  //       type: ['circle', 'square'],
-  //       options: {},
-  //     },
-  //   },
-  //   responsive: [
-  //     {
-  //       maxWidth: 1024,
-  //       options: {
-  //         particles: {
-  //           move: {
-  //             speed: {
-  //               min: 33,
-  //               max: 66,
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   ],
-  // };
-
-  async loadConfetti(engine: Engine) {
-    await loadConfettiPreset(engine);
+  async loadSnowPreset(engine: Engine) {
+    await loadSnowPreset(engine);
   }
 
   loadedCallback(engine: Engine) {
